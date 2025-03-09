e8198769cac249ebec81e4242a7bd1c0c4770aa9
jjmuchai <jjmuchai151@gmail.com>
2025-03-09 14:06:08 +0300

    My updates to the project

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your code in this file!
+// const currentUser = 'Grace Hopper';
+
+const currentUser = 'James Hopper';
+
+// const welcomeMessage = 'Welcome to Flatbook, ';
+// const welcomeMessage = currentUser;
+// const welcomeMessage = 'Welcome to Flatbook, currentUser';
+// const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// const shortGreeting = 'Welcome, ';
+// const shortGreeting = `Welcome, ${currentUser}`;
+// const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}`;
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
